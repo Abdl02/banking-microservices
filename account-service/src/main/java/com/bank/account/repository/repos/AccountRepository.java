dc91f907d2291285da988fb3610d36ad494373ea
Abdel Rahman Jebril <abdjibrel@gmail.com>
2025-02-24 09:25:17 +0300

    JDBQ

--- account-service/src/main/java/com/bank/account/repository/repos/AccountRepository.java
+++ account-service/src/main/java/com/bank/account/repository/repos/AccountRepository.java
@@ -1,10 +1,15 @@
 package com.bank.account.repository.repos;
 
 import com.bank.account.repository.entity.Account;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByCustomerId(Long customerId);
+
+    @Query("SELECT a FROM Account a WHERE a.status = :status")
+    List<Account> findByStatus(@Param("status") String status);
 }
